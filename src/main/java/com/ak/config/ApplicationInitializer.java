12c7dd5b42a307089de2b26c0d641c780a7f3127
Dmytro Bartoshchuk <dmitriy.bartoshchuk@gmail.com>
2017-04-04 17:58:02 +0200

    added configurations

--- src/main/java/com/ak/config/ApplicationInitializer.java
+++ src/main/java/com/ak/config/ApplicationInitializer.java
@@ -1,5 +1,34 @@
 package main.java.com.ak.config;
 
-public class ApplicationInitializer {
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
 
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.servlet.DispatcherServlet;
+
+public class ApplicationInitializer implements WebApplicationInitializer{
+
+	@Override
+	public void onStartup(ServletContext servletContext) throws ServletException {
+		
+		AnnotationConfigWebApplicationContext applicationConfig = new AnnotationConfigWebApplicationContext();
+		applicationConfig.register(AppConfig.class);
+		
+		DispatcherServlet dispatcherServlet = new DispatcherServlet(applicationConfig);
+		
+		ServletRegistration.Dynamic servletRegistration = servletContext.addServlet("dispatcher", dispatcherServlet);
+		
+		servletRegistration.setLoadOnStartup(1);
+		servletRegistration.addMapping("/");
+		
+		CharacterEncodingFilter charEnc = new CharacterEncodingFilter();
+		charEnc.setEncoding("UTF-8");
+		charEnc.setForceEncoding(true);
+		servletContext.addFilter(null, "/*").addMappingForUrlPatterns(null, true, "/*");
+	}
+	
+	
 }
